a4b7b8dc5c974e8e0036b980134d27caa3f071da
Georgios Antonopoulos <g.ant@endiag.com>
2021-03-13 12:54:16 +0100

    remove netbeans project things and obsolete dirs

--- build/Debug/MinGW-Windows/src/tiles/tile_dimension_exception.o.d
+++
@@ -1,8 +0,0 @@
-build/Debug/MinGW-Windows/src/tiles/tile_dimension_exception.o: \
- src/tiles/tile_dimension_exception.cpp \
- include/tiles/tile_dimension_exception.h \
- c:/Software/boost_1_56_0/boost/exception/all.hpp
-
-include/tiles/tile_dimension_exception.h:
-
-c:/Software/boost_1_56_0/boost/exception/all.hpp:
